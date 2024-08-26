48c4f603aafe66fd786da470cb39572fda56f253
tnazarew <tomasz.nazarewicz@getindata.com>
2024-08-26 10:11:41 +0200

    put validator in workflow
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- consumer/consumers/Dataplex/validator/ol_test.py
+++ consumer/consumers/Dataplex/validator/ol_test.py
@@ -80,56 +80,34 @@ def setup_jinja() -> Environment:
 env = setup_jinja()
 
 
-def match(expected, result) -> (bool, list):
+def match(expected, result, prefix) -> list:
     """
     Check if result is "equal" to expected value.
     """
+    errors = []
     if isinstance(expected, dict):
         # Take a look only at keys present at expected dictionary
         for k, v in expected.items():
             if k not in result:
-                return False, [f"Key {k} not in received event {result}\nExpected {expected}"]
-            matched, errors = match(v, result[k])
-            if not matched:
-                errors.append([f"For key {k}, expected value {v} not equals received {result[k]}" + f"\nExpected {expected}, request {result}"])
-                return False, errors
+                errors.append(f"Key {prefix}.{k} missing")
+            else:
+                errors.extend(match(v, result[k], f"{prefix}.{k}"))
     elif isinstance(expected, list):
         if len(expected) != len(result):
-            return False, [f"Length does not match: expected {len(expected)} result: {len(result)}"]
-
-        # Try to resolve case where we have wrongly sorted lists by looking at name attr
-        # If name is not present then assume that lists are sorted
-        for i, x in enumerate(expected):
-            if "name" in x:
-                matched = False
-                for y in result:
-                    if "name" in y:
-                        expected_name = env.from_string(x["name"]).render()
-                        result_name = env.from_string(y["name"]).render()
-                        if expected_name == result_name:
-                            matched, errors = match(x, y)
-                            if not matched:
-                                errors.append([f"List not matched {x} where {y}"])
-                                return False, errors
-                    matched = True
-                    break
-                if not matched:
-                    return False, [f"List not matched - no same stuff for {x} " + f"in expected: {expected} result {result}"]
-            else:
-                matched, errors = match(x, result[i])
-                if not matched:
-                    errors.append([f"List not matched at {i}\n" + f"  expected:\n{json.dumps(x)}\n" + f"  result: \n{json.dumps(result[i])}"])
-                    return False, errors
+            errors.append(f"In {prefix}: Length does not match: expected {len(expected)} result: {len(result)}")
+        else:
+            for i, x in enumerate(expected):
+                errors.extend(match(x, result[i], f"{prefix}.[{i}]"))
     elif isinstance(expected, str):
         if "{{" in expected:
             # Evaluate jinja: in some cases, we want to check only if key exists, or if
             # value has the right type
             rendered = env.from_string(expected).render(result=result)
-            if rendered == "true" or rendered == result:
-                return True, []
-            return False, [f"Rendered value {rendered} does not equal 'true' or {result}"]
+            if not (rendered == "true" or rendered == result):
+                errors.append(f"In {prefix}: Rendered value {rendered} does not equal 'true' or {result}")
         elif expected != result:
-            return False, [f"Expected value {expected} does not equal result {result}"]
+            errors.append(f"In {prefix}: Expected value {expected} does not equal result {result}")
     elif expected != result:
-        return False, [f"Object of type {type(expected)}: {expected} does not match {result}"]
-    return True, []
+        errors.append(f"In {prefix}: Object of type {type(expected)}: {expected} does not match {result}")
+
+    return errors
--- consumer/consumers/Dataplex/validator/validator.py
+++ consumer/consumers/Dataplex/validator/validator.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 
@@ -8,134 +9,140 @@ import proto
 from ol_test import match
 from google.protobuf.json_format import ParseDict
 from google.protobuf import struct_pb2
-import configparser
 
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-default = config['DEFAULT']
+class Validator:
+    def __init__(self, client=None, consumer_dir=None, scenario_dir=None, parent=None):
+        self.client = client
+        self.consumer_dir = consumer_dir
+        self.scenario_dir = scenario_dir
+        self.parent = parent
 
-CONSUMER_DIR = default['consumerDir']
-SCENARIO_DIR = default['scenarioDir']
-PARENT = default['parent']
-CLIENT = LineageClient(credentials=Credentials.from_service_account_file(default['credentialFileLocation']))
+    def load_ol_events(self, scenario):
+        return [{'name': entry.name, 'payload': ParseDict(json.load(open(entry.path, 'r')), struct_pb2.Struct())}
+                for entry in os.scandir(f"{self.scenario_dir}/{scenario}/events") if entry.is_file()]
 
+    def load_validation_events(self, scenario):
+        processes = json.load(open(f"{self.consumer_dir}/scenarios/{scenario}/validation/processes.json", 'r'))
+        runs = json.load(open(f"{self.consumer_dir}/scenarios/{scenario}/validation/runs.json", 'r'))
+        lineage_events = json.load(open(f"{self.consumer_dir}/scenarios/{scenario}/validation/lineage_events.json", 'r'))
+        return processes, runs, lineage_events
 
-class ScenariosUtils:
-    pass
+    def send_events(self, scenario):
+        events = self.load_ol_events(scenario)
+        report = []
+        for e in events:
+            try:
+                response = self.client.process_open_lineage_run_event(parent=self.parent, open_lineage=e['payload'])
+                report.append({"status": "SUCCESS", 'name': e['name']})
+            except InvalidArgument as exc:
+                report.append({"status": "FAILURE", 'validation': 'syntax', "details": exc.args[0], 'name': e['name']})
+        return report
 
+    def validate(self, scenario):
+        self.clean_up()
+        report = self.send_events(scenario)
+        if not any(r['status'] == "FAILURE" for r in report):
+            report.extend( self.validate_api_state(scenario))
+        for r in report:
+            r['scenario'] = scenario
+        self.clean_up()
+        return report
 
-class Report:
-    pass
+    def get_api_state(self):
+        processes = [proto.Message.to_dict(p) for p in self.client.list_processes(parent=self.parent)]
+        runs = [proto.Message.to_dict(r) for p in processes for r in self.client.list_runs(parent=p['name'])]
+        lineage_events = [proto.Message.to_dict(e) for r in runs for e in self.client.list_lineage_events(parent=r['name'])]
+        return processes, runs, lineage_events
 
+    def validate_api_state(self, scenario):
+        processes_expected, runs_expected, events_expected = self.load_validation_events(scenario)
+        processes_state, runs_state, events_state = self.get_api_state()
+        report = []
+        report.extend(self.compare_process_or_run(processes_expected, processes_state, 'process'))
+        report.extend(self.compare_process_or_run(runs_expected, runs_state, 'run'))
+        report.extend(self.compare_lineage_events(events_expected, events_state))
 
-class ApiUtils:
-    pass
+        return report
 
+    def clean_up(self):
+        processes = [x for x in self.client.list_processes(parent=self.parent)]
+        for p in processes:
+            self.client.delete_process(name=p.name)
 
-def list_scenarios():
-    return [entry.name for entry in os.scandir(f"{CONSUMER_DIR}/scenarios") if entry.is_dir()]
-
-
-def load_ol_events(scenario):
-    return [{'name': entry.name, 'payload': ParseDict(json.load(open(entry.path, 'r')), struct_pb2.Struct())} for entry
-            in os.scandir(f"{SCENARIO_DIR}/{scenario}/events") if entry.is_file()]
-
-
-def load_validation_events(scenario):
-    processes = json.load(open(f"{CONSUMER_DIR}/scenarios/{scenario}/validation/processes.json", 'r'))
-    runs = json.load(open(f"{CONSUMER_DIR}/scenarios/{scenario}/validation/runs.json", 'r'))
-    lineage_events = json.load(open(f"{CONSUMER_DIR}/scenarios/{scenario}/validation/lineage_events.json", 'r'))
-    return processes, runs, lineage_events
-
-
-def clean_up():
-    processes = [x for x in CLIENT.list_processes(parent=PARENT)]
-    for p in processes:
-        CLIENT.delete_process(name=p.name)
-
-
-def send_events(scenario):
-    events = load_ol_events(scenario)
-    for e in events:
-        try:
-            response = CLIENT.process_open_lineage_run_event(parent=PARENT, open_lineage=e['payload'])
-            e['report'] = {"status": "SUCCESS", "process": response.process, "run": response.run,
-                           "lineage_events": [x for x in response.lineage_events]}
-        except InvalidArgument as exc:
-            e['report'] = {"status": "FAILURE", "code": exc.code, "details": exc.args[0]}
-    return events
-
-
-def get_api_state():
-    processes = [proto.Message.to_dict(p) for p in CLIENT.list_processes(parent=PARENT)]
-    runs = [proto.Message.to_dict(r) for p in processes for r in CLIENT.list_runs(parent=p['name'])]
-    lineage_events = [proto.Message.to_dict(e) for r in runs for e in CLIENT.list_lineage_events(parent=r['name'])]
-    return processes, runs, lineage_events
-
-
-def validate_api_state(scenario):
-    processes_expected, runs_expected, events_expected = load_validation_events(scenario)
-    processes_result, runs_result, events_result = get_api_state()
-    report = [compare_process_or_run(processes_expected, processes_result),
-              compare_process_or_run(runs_expected, runs_result),
-              compare_lineage_events(events_expected, events_result)]
-    return report
-
-
-def compare_dicts(d):
-    for v in d.values():
-        v['val'] = False, "no matching entity"
-        if v.__contains__('expected') and v.__contains__('result'):
-            v['val'] = match(v['expected'], v['result'])
-    return d
-
-
-# processes and runs are matchable by entity name
-def compare_process_or_run(expected, result):
-    d = {}
-    for e in expected:
-        d.setdefault(e['name'], {})['expected'] = e
-    for r in result:
-        d.setdefault(r['name'], {})['result'] = r
-    return compare_dicts(d)
-
-
-# lineage events can't be matched by name, so they're matched by equal start and end time
-def compare_lineage_events(expected, result):
-    d = {}
-    for r in result:
-        d.setdefault(r['name'], {})['result'] = r
-    for e in expected:
-        matching = next((r for r in result if e['start_time'] == r['start_time'] and e['end_time'] == r['end_time']),
-                        None)
-        if matching is not None:
-            d[matching['name']]['expected'] = e
-        else:
+    # processes and runs are matchable by entity name
+    def compare_process_or_run(self, expected, result, entity_type):
+        d = {}
+        for e in expected:
             d.setdefault(e['name'], {})['expected'] = e
-    return compare_dicts(d)
+        for r in result:
+            d.setdefault(r['name'], {})['result'] = r
+        results = []
+        for k, v in d.items():
+            result = ["no matching entity"]
+            if v.__contains__('expected') and v.__contains__('result'):
+                result = match(v['expected'], v['result'], "")
+            results.append({'type': entity_type, 'status': 'SUCCESS' if len(result) == 0 else 'FAILURE',
+                            'details': result, 'validation': 'semantics', 'name': k})
+        return results
+
+    # lineage events can't be matched by name, so they're matched by equal start and end time
+    def compare_lineage_events(self, expected, result):
+        d = {}
+        for r in result:
+            d.setdefault(r['name'], {})['result'] = r
+        for e in expected:
+            matching = next(
+                (r for r in result if e['start_time'] == r['start_time'] and e['end_time'] == r['end_time']),
+                None)
+            if matching is not None:
+                d[matching['name']]['expected'] = e
+            else:
+                d.setdefault(e['name'], {})['expected'] = e
+        results = []
+        for k, v in d.items():
+            result = ["no matching entity"]
+            if v.__contains__('expected') and v.__contains__('result'):
+                result = match(v['expected']['links'], v['result']['links'], ".links")
+            results.append({'type': 'event', 'status': 'SUCCESS' if len(result) == 0 else 'FAILURE',
+                            'details': result, 'validation': 'semantics', 'name': k})
+        return results
+
+    def __repr__(self):
+        return (f"MyClass(client={self.client}, consumer_dir={self.consumer_dir}, "
+                f"scenario_dir={self.scenario_dir}, parent={self.parent})")
 
 
-def compile_report(syntax_report, semantic_report):
-    print("dupa")
+def list_scenarios(consumer_dir):
+    return [entry.name for entry in os.scandir(f"{consumer_dir}/scenarios") if entry.is_dir()]
 
 
-def validate(scenario):
-    clean_up()
-    syntax_report = send_events(scenario)
-    semantic_report = []
-    if not any(event['report']['status'] == "FAILURE" for event in syntax_report):
-        semantic_report = validate_api_state(scenario)
-    report = compile_report(syntax_report, semantic_report)
-    clean_up()
+def get_arguments():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('--credentials', type=str, help="credentials for GCP", default=None)
+    parser.add_argument('--consumer_dir', type=str, help="Path to the consumer directory", default=None)
+    parser.add_argument('--scenario_dir', type=str, help="Path to the scenario directory", default=None)
+    parser.add_argument('--parent', type=str, help="Parent identifier", default=None)
+
+    args = parser.parse_args()
+
+    client = LineageClient(credentials=Credentials.from_service_account_file(args.credentials))
+    consumer_dir = args.consumer_dir
+    scenario_dir = args.scenario_dir
+    parent = args.parent
+
+    return consumer_dir, scenario_dir, parent, client
 
 
 def main():
-    scenarios = list_scenarios()
-    for scenario in scenarios:
-        validate(scenario)
+    consumer_dir, scenario_dir, parent, client = get_arguments()
+    validator = Validator(client, consumer_dir, scenario_dir, parent)
+    scenarios = list_scenarios(consumer_dir)
+    reports = [validator.validate(scenario) for scenario in scenarios]
+    json.dump(reports, open('report.json', 'w'))
 
 
 if __name__ == "__main__":
     main()
-    # print('a')
+
+
