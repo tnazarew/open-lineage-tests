b1f9529e9bbe3b4878cd7b452377d1ffff52c87d
Dominik Dębowczyk <dominik.debowczyk@getindata.com>
2024-10-23 16:19:09 +0200

    Download bq connector from maven

--- producer/spark_dataproc/scenarios/bigquery_input/test/bigquery_input.py
+++ producer/spark_dataproc/scenarios/bigquery_input/test/bigquery_input.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import time
 
 from pyspark.sql import SparkSession
 
@@ -14,4 +13,4 @@ df = df.select('word', 'word_count')
 df = df.where("word_count > 0 AND word='spark'")
 df = df.groupBy('word').sum('word_count')
 
-df.coalesce(1).write.csv(f"file:///tmp/outputs/{round(time.time() * 1000)}")
+df.coalesce(1).write.csv(f"file:///tmp/my_shakespeare_output")
