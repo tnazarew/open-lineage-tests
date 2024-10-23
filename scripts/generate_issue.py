e60a3f93b94ffe186affacce054256f2ac450f2b
tnazarew <tomasz.nazarewicz@getindata.com>
2024-10-23 23:34:57 +0200

    another lot of changes
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- scripts/generate_issue.py
+++ scripts/generate_issue.py
@@ -3,6 +3,38 @@ import json
 from os.path import isfile, join
 
 
+def get_failures(components):
+    result = ''
+    for component in components:
+        result += f"## Component: {component['name']}\n"
+        component_path = get_component_path(component)
+        consumer_github_maintainers = [maintainer.get('github-name') for maintainer in json.load(
+            open(join(component_path, 'maintainers.json'), 'r'))
+                                       if maintainer.get('github-name') is not None]
+        result += "Maintainers for component: " + ' '.join([f"@{cgm}" for cgm in consumer_github_maintainers]) + '  \n'
+        for failed in component['scenarios']:
+            result += f"### Scenario: {failed['name']}\n"
+            scenario_github_maintainers = [maintainer.get('github-name') for maintainer in
+                json.load(open(get_scenario_path(component_path, failed), 'r')) if maintainer.get('github-name') is not None]
+            result += "Maintainers for scenario: " + ' '.join(
+                [f"@{cgm}" for cgm in scenario_github_maintainers if cgm not in consumer_github_maintainers]) + '  \n'
+            # result += f"failures in: \n"
+            result += '\n'.join(
+                [f"name: {t['name']},  \nvalidation_type: {t['validation_type']}  \nentity_type: {t['entity_type']}  \ndetails:  \n\t" +
+                 '  \n\t'.join([f"`{d}`" for d in t['details']]) for t in failed['tests']])
+    return result
+
+
+def get_scenario_path(component_path, failed):
+    return join(component_path, 'scenarios', failed['name'], 'maintainers.json')
+
+
+def get_component_path(component):
+    if component['component_type'] == 'consumer':
+        return join('consumer', 'consumers', component['name'])
+    return join('producer', component['name'])
+
+
 def get_arguments():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument('--failure_path', type=str, help="directory containing the failures file")
@@ -10,25 +42,6 @@ def get_arguments():
     args = parser.parse_args()
 
     return args.failure_path, args.issue_path
-
-
-def get_failures(components):
-    result = ''
-    for component in components:
-        result += f"## Component: {component['name']}\n"
-        consumer_github_maintainers = [maintainer.get('github-name') for maintainer in json.load(
-            open(join('consumer', 'consumers', component['name'], 'maintainers.json'), 'r'))
-                                       if maintainer.get('github-name') is not None]
-        result += "Maintainers for component: " + ' '.join([f"@{cgm}" for cgm in consumer_github_maintainers]) + '  \n'
-        for failed in component['scenarios']:
-            result += f"### Scenario: {failed['name']}\n"
-            scenario_github_maintainers = [maintainer.get('github-name') for maintainer in json.load(
-                open(join('consumer', 'consumers', component['name'], 'scenarios', failed['name'],
-                          'maintainers.json'), 'r')) if maintainer.get('github-name') is not None]
-            result += "Maintainers for scenario: " + ' '.join([f"@{cgm}" for cgm in scenario_github_maintainers if cgm not in consumer_github_maintainers]) + '  \n'
-            # result += f"failures in: \n"
-            result += '\n'.join([f"name: {t['name']},  \nvalidation_type: {t['validation_type']}  \nentity_type: {t['entity_type']}  \ndetails:  \n\t" + '  \n\t'.join([f"`{d}`" for d in t['details']]) for t in failed['tests']])
-    return result
 
 
 def main():
