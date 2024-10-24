cb6eba77e31bae790f9e0e4ef650e4b50ec768fd
Dominik Dębowczyk <dominik.debowczyk@getindata.com>
2024-10-24 12:31:00 +0200

    Fix initialization script

--- producer/spark_dataproc/runner/get_openlineage_jar.sh
+++ producer/spark_dataproc/runner/get_openlineage_jar.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Installs OpenLineage jar
-# onto a Cloud Dataproc cluster.
+# Installs OpenLineage jar onto a Cloud Dataproc cluster.
 
 set -euxo pipefail
 
 readonly VM_SPARK_JARS_DIR=/usr/lib/spark/jars
-readonly OPENLINEAGE_SPARK_URL=$(/usr/share/google/get_metadata_value attributes/OPENLINEAGE_SPARK_URL || true)
+readonly OPENLINEAGE_SPARK_URL=$(/usr/share/google/get_metadata_value attributes/OPENLINEAGE_SPARK_URL || echo "")
 
-if [[ -d ${OPENLINEAGE_SPARK_URL} ]]; then
-    jar_url=${OPENLINEAGE_SPARK_URL}
+if [[ -n "${OPENLINEAGE_SPARK_URL}" ]]; then
+    jar_url="${OPENLINEAGE_SPARK_URL}"
 else
-    jar_url=gs://open-lineage-e2e/jars/openlineage-spark_2.12-1.23.0.jar
+    jar_url="gs://open-lineage-e2e/jars/openlineage-spark_2.12-1.23.0.jar"
 fi
 
 gsutil cp -P "${jar_url}" "${VM_SPARK_JARS_DIR}/"
