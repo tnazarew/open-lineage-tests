dcaf71bf250880a0ab029110568f17ca9daaf2d7
tnazarew <tomasz.nazarewicz@getindata.com>
2024-10-24 07:10:40 +0200

    add logs and workflow_dispatch to new release build
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- scripts/upload_file_to_gcs.py
+++ scripts/upload_file_to_gcs.py
@@ -33,7 +33,7 @@ def main():
     if not os.path.exists(local_file_path):
         print(f"file {local_file_path} not found")
         # Local file does not exist; do nothing
-        sys.exit(0)
+        sys.exit(1)
 
     if not gcs_path.startswith('gs://'):
         print("Error: GCS path must start with gs://")
