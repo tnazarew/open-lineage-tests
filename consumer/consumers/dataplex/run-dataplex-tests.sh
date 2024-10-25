fe84c7e8b6cb0c7724f1173fa6ff278e1b6f240c
tnazarew <tomasz.nazarewicz@getindata.com>
2024-10-25 05:34:37 +0200

    local scripts and slight refactor of wrokflows
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- consumer/consumers/dataplex/run-dataplex-tests.sh
+++ consumer/consumers/dataplex/run-dataplex-tests.sh
@@ -1,5 +1,48 @@
-#TODO add mention of setting up python 3.11
-#TODO add mention about already having dataplex setup
+usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo ""
+    echo "Options:"
+    echo "  --gcp-credentials-json-path PATH    Path to GCP credentials JSON (required)"
+    echo "  --consumer-dir PATH                 Scenario path (default:)"
+    echo "  --scenario-dir PATH                 Consumer path (default:) "
+    echo "  --gcp-parent PATH                   GCP parent (required)"
+    echo "  --dump-api-state                    Flag to dump API state (default: false)"
+    echo "  -h, --help                          Show this help message and exit"
+    echo ""
+    echo "Example:"
+    echo "  $0 --gcp-credentials-json-path /path/to/credentials.json --gcs-transport-jar-path /path/to/jar --dump-api-state"
+    exit 0
+}
+
+GCP_CREDENTIALS_JSON_PATH=""
+CONSUMER_DIR="./"
+SCENARIO_DIR="../scenarios"
+GCP_PARENT=""
+DUMP_API_STATE=""
+
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    usage
+fi
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --gcp-credentials-json-path) GCP_CREDENTIALS_JSON_PATH="$2"; shift ;;
+        --consumer-dir) CONSUMER_DIR="$2"; shift ;;
+        --scenario-dir) SCENARIO_DIR="$2"; shift ;;
+        --gcp-parent) GCP_PARENT="$2"; shift ;;
+        --dump-api-state) DUMP_API_STATE='--dump' ;;
+        *) echo "Unknown parameter passed: $1"; usage ;;
+    esac
+    shift
+done
+
+# Check required arguments
+if [[ -z "$GCP_CREDENTIALS_JSON_PATH" || -z "$GCP_PARENT" ]]; then
+    echo "Error: Missing required arguments."
+    usage
+fi
+
+[[ "$(python3 --version 2>&1)" =~ ^Python\ 3\.11\.* ]] || { echo "Python version 3.11.x required. Detected: $PYTHON_VERSION"; exit 1; }
 
 python -m pip install --upgrade pip
 pip install flake8 pytest
@@ -7,7 +50,7 @@ if [ -f consumer/consumers/dataplex/validator/requirements.txt ]; then pip insta
 
 
 python consumer/consumers/dataplex/validator/validator.py \
-  --credentials ${{ steps.gcp-auth.outputs.credentials_file_path }} \
-  --consumer_dir consumer/consumers/dataplex \
-  --scenario_dir consumer/scenarios/ \
-  --parent projects/gcp-open-lineage-testing/locations/us
+  --credentials "$GCP_CREDENTIALS_JSON_PATH" \
+  --consumer_dir "$CONSUMER_DIR" \
+  --scenario_dir "$SCENARIO_DIR" \
+  --parent "$GCP_PARENT" ${DUMP_API_STATE}
